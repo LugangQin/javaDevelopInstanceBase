2f8e6ac338de62249d9017a7446d05a6b63df395
zhangsan <2660579369@qq.com>
2022-12-18 14:56:41 +0800

    开始学习java开发实例大全

---
+++ src/main/java/com/javabase/test/test2.java
@@ -0,0 +1,10 @@
+package com.javabase.test;
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("aeg");
+        System.out.println("sgsg");
+        System.out.println("asegaegas");
+        System.out.println("测试终端");
+    }
+}
